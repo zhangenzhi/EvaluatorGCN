2797fdda4aca2efae69bb2f85fc7cf27a521e4d9
zhangenzhi <zhangenzhi657@gmail.com>
2019-12-17 00:45:31 +0900

    offline GCN evaluator by numpy

--- Eva_GCN.py
+++ Eva_GCN.py
@@ -5,6 +5,7 @@ from __future__ import print_function
 import sys
 import os
 import time
+import pickle
 
 import numpy as np
 import tensorflow as tf
@@ -25,8 +26,10 @@ class evaluator(object):
         self.num_of_nodes = num_of_nodes
         self.num_of_branches = num_of_branches
         self.whole_channels = whole_channels
+
         self._create_params()
         self._build_train()
+
 
     def _create_params(self):
 
@@ -65,7 +68,7 @@ class evaluator(object):
                     b = tf.get_variable("b",[1,1])
                     self.w_nn.append(w)
                     self.b_nn.append(b)
-
+                  
 
     def _build_train(self):
 
@@ -128,6 +131,13 @@ class evaluator(object):
                 test_losses.append(test_loss)
                 print("epoch:",i,"mean_square_loss:",losss,"validation loss:", val_loss,"test_loss:",test_loss)
             saver.save(sess,'./checkpoint_dir/evaluator')
+
+            w_gcn = sess.run(self.w_gcn)
+            w_nn = sess.run(self.w_nn)
+            b_nn = sess.run(self.b_nn)
+            with open("Data/weight_gcn","wb") as f:
+                weights = {"w_gcn":w_gcn,"w_nn":w_nn,"b_nn":b_nn}
+                pickle.dump(weights,f)
             
 
     def _model(self,supports,features):
@@ -189,18 +199,86 @@ class evaluator(object):
         return eval_acc[0][0]
         # return 0
 
+
 class offline_evaluator(object):
-    def __init__(self):
-        pass
-    def _load_model_from_tf(self):
-        pass
-    def _build_model_by_np(self):
-        pass
+
+    def __init__(self,
+        num_of_nodes,
+        whole_channels,
+        num_of_branches):
+
+        self.gcn_num_layers = 2
+        self.gcn_size = num_of_nodes
+        self.name = "evaluator"
+        self.num_of_nodes = num_of_nodes
+        self.num_of_branches = num_of_branches
+        self.whole_channels = whole_channels
+
+        self.w_gcn = []
+        self.w_nn = []
+        self.b_nn = []
+
+        self._load_weighs()
+
+    def sigmoid(self,x):
+        s = 1 / (1 + np.exp(-x))
+        return s
+
+    def relu(self,x):
+        s = np.where(x < 0, 0, x)
+        return s
+
+    def _load_weighs(self):
+        with open("Data/weight_gcn","rb") as f:
+            weights = pickle.load(f)
+            self.w_gcn = weights["w_gcn"]
+            self.w_nn = weights["w_nn"]
+            self.b_nn = weights["b_nn"]
+
+    def _build_model_by_np(self,supports,features):
+
+
+        supports_1 = np.matmul(supports,features) # None 12 6
+        weight_1 = self.w_gcn[0] # 6,12
+        result_1 = self.relu(np.matmul(np.reshape(supports_1,[-1,6]),weight_1))
+
+
+        support_2 = np.matmul(supports,np.reshape(result_1,[-1,12,12]))
+        weight_2 = self.w_gcn[1]
+        result_2 = self.relu(np.matmul(np.reshape(support_2,[-1,12]),weight_2))
+        result_2 = np.reshape(result_2,[-1,12,24])
+
+        flatten = result_2.flatten()
+        dense_1 = self.relu(np.matmul(flatten,self.w_nn[0]) + self.b_nn[0])
+        dense_2 = self.relu(np.matmul(dense_1,self.w_nn[1]) + self.b_nn[1])
+        dense_3 = self.relu(np.matmul(dense_2,self.w_nn[2]) + self.b_nn[2])
+        accuracy = self.sigmoid(np.matmul(dense_3,self.w_nn[3]) + self.b_nn[3])
+
+        return accuracy
+
     def evaluate(self,arc):
-        pass
-    
+        
+        submodel = arc
+        A = np.zeros(shape = (self.num_of_nodes,self.num_of_nodes))
+        D = np.zeros(shape = (self.num_of_nodes,self.num_of_nodes))
+        feature = np.zeros(shape=(self.num_of_nodes,6))
+        for i in range(self.num_of_nodes):
+            layer = submodel[i][1:]
+            feature[i][submodel[i][0]] = 1
+            for j in range(len(layer)):
+                A[i][j] = layer[j]
+        A = A + np.transpose(A)
+        for i in range(self.num_of_nodes):
+            D[i][i] = sum(A[i])
+        supports = np.eye(self.num_of_nodes,self.num_of_nodes) - np.matmul(np.matmul(np.sqrt(D),A),np.sqrt(D))
+        # supports = np.matmul(supports,feature)
+
+        eval_acc = self._build_model_by_np(supports,feature)
+        return eval_acc[0][0]
+
 if __name__ == "__main__":
     eval_model = evaluator(12,True,8)
+    offline_eval_model = offline_evaluator(12,True,8) 
     arc = [
         [0],
         [0,0],
@@ -216,6 +294,7 @@ if __name__ == "__main__":
         [2,0,1,1,0,0,1,0,1,1,1,0]]
 
     print(eval_model.evaluate(arc))
+    print(offline_eval_model.evaluate(arc))
 
     # with tf.Session() as sess:
     #     sess.run(tf.global_variables_initializer())
